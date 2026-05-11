f85541ae49ec5ac8b567c660b6142582e4b493a2
anasnaeemqu <136015453+anasnaeemqu@users.noreply.github.com>
2026-05-12 02:22:28 +0500

    Updated Api

--- Marketplace.API/Program.cs
+++ Marketplace.API/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddCors(options =>
         policy.AllowAnyOrigin()
               .AllowAnyMethod()
               .AllowAnyHeader();
+              //.AllowCredentials();
     });
 });
 
